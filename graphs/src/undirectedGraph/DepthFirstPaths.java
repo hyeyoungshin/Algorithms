33d394b2b9e131ccbec6ee27fe5fccb5e5a5c96e
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-05-22 16:22:55 -0500

    added dfp class

---
+++ graphs/src/undirectedGraph/DepthFirstPaths.java
@@ -0,0 +1,25 @@
+package undirectedGraph;
+
+public class DepthFirstPaths {
+	private boolean[] marked;
+	private int[] edgeTo;
+	private int s;
+	
+	public DepthFirstPaths(Graph G, int s)
+	{
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		dfs(G, s);
+	}
+	
+	private void dfs(Graph G, int v)
+	{
+		marked[v] = true;
+		for (int w: G.adj(v))
+			if (!marked[w])
+			{
+				dfs(G, w);
+				edgeTo[w] = v;
+			}
+	}
+}
