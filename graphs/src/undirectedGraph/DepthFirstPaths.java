5386f91263eced458d296437937d8c67c4cb768c
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-05-22 16:25:03 -0500

    added some comments

--- graphs/src/undirectedGraph/DepthFirstPaths.java
+++ graphs/src/undirectedGraph/DepthFirstPaths.java
@@ -1,18 +1,18 @@
 package undirectedGraph;
 
 public class DepthFirstPaths {
-	private boolean[] marked;
-	private int[] edgeTo;
+	private boolean[] marked; // true if v connected to s
+	private int[] edgeTo; // previous vertex on path from s to v
 	private int s;
 	
 	public DepthFirstPaths(Graph G, int s)
 	{
 		marked = new boolean[G.V()];
 		edgeTo = new int[G.V()];
-		dfs(G, s);
+		dfs(G, s); // find vertices connected to s
 	}
 	
-	private void dfs(Graph G, int v)
+	private void dfs(Graph G, int v) // recursive DFS does the work
 	{
 		marked[v] = true;
 		for (int w: G.adj(v))
