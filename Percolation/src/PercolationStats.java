3b5e9fada2272613b231ade6d2a1e691e4c52c8c
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-07-06 00:51:42 -0500

    finished but untested

--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class PercolationStats {
 	
 	private int n; // N in N-by-N grid
-	private int exp;
+	private int trials;
 	private ArrayList<Integer> openSites; // number of open sites for each experiment
 
 	public PercolationStats(int N, int T) // perform T independent experiments on an N-by-N grid  
@@ -14,34 +14,50 @@ public class PercolationStats {
 			throw new IllegalArgumentException();
 		
 		this.n = N;
-		exp = T;
+		trials = T;
+		int count = 0;
 		
 		Random rand = new Random();
 		int row = rand.nextInt(n) + 1;
 		int col = rand.nextInt(n) + 1;
 		
 		Percolation perc = new Percolation(n);
-		while(exp != 0) {
+		while(count != trials) {
 			perc.open(row, col);
-			exp--;
+			count++;
 		}
 	}
 	public double mean() // sample mean of percolation threshold
 	{
-		return 0.0;
+		int sum = 0;
+		for(int i=0; i < trials; i++)
+		{
+			sum += openSites.get(i) / (n*n);
+		}
+		
+		return sum / trials;
 	}
 	public double stddev() // sample standard deviation of percolation threshold
 	{
-		return 0.0;
+		
+		int sumSquars = 0;
+		for(int i=0; i<trials; i++){
+			sumSquars += (openSites.get(i) / (n*n))^2;
+		}
+		
+		double m = mean();
+		
+		return Math.sqrt((sumSquars - (trials*(m*m)))/(trials-1));
 	}
 	public double confidenceLo() //low end point of 95% confidence interval
 	{
-		return 0.0;
+		return mean()-((1.96*stddev())/Math.sqrt(trials));
 	}
 	public double confidenceHi() // high end point of 95% confidence interval
 	{
-		return 0.0;
+		return mean()+((1.96*stddev())/Math.sqrt(trials));
 	}
+	
 	public static void main(String[] args) // test client (described below)
 	{
 		
